a17fe3f323663b2a0fbeb6801e65f1317fee4ae0
JiaC-Lin <jiacong.d.lin@gmail.com>
2023-04-13 17:05:03 -0400

    Product of array except itself

--- Arrays & Hashing/5-Top-K-Frequent-Elements.py
+++ Arrays & Hashing/5-Top-K-Frequent-Elements.py
@@ -19,7 +19,7 @@ class Solution(object):
             if n not in countDict: # skips recurring values; they're already counted
                 value = nums.count(n)
                 countDict[n] = value
-        print(f"{countDict}")
+        # print(f"{countDict}")
 
         lenK = k # save copy of k
         i = 0
@@ -35,29 +35,35 @@ class Solution(object):
                         output[i] = key
                         key = numValue
                         # Then continue to compare with the old numValue with the rest of the output array; countDict.get(numvalue) < countDict.get(oldNumValue)
-        print(f"{output}")
+        # print(f"{output}")
         return output
     
+# Option 2: make a list of size N where the indexes will be the count and the elements will contain the element(s) with count equal to the index; runs in O(n)
     def neetTopKFrequent(self, nums: List[int], k: int) -> List[int]:
         """
         :type nums: List[int]
         :type k: int
         :rtype: List[int]
         """ 
-        count = {}
-        freq = [[] for i in range(len(nums) + 1)]
+        count = {} # (number: count)
+        # make a list with size N (in case all numbers are same and highest count is N)
+        freq = [[] for i in range(len(nums) + 1)] # index is going to be count and elements contains [number(s)]
 
-        for n in nums:
+        for n in nums: # count occurence of nums 
             count[n] = 1 + count.get(n, 0)
+        # Flip dict and fill in the freq list
         for n, c in count.items():
             freq[c].append(n)
+        # print(f"{freq}")
 
-        res = []
-        for i in range(len(freq) - 1, 0, -1):
+        output = []
+        # decrement from len(freq) - 1 to 0 to get most frequent element, do it k times
+        for i in range(len(freq) - 1, 0, -1): 
             for n in freq[i]:
-                res.append(n)
-                if len(res) == k:
-                    return res
+                output.append(n)
+                if len(output) == k: # stopping case
+                    # print(f"{output}")
+                    return output
 
 
 test = Solution()
@@ -68,7 +74,7 @@ num1 = [1, 1, 1, 2, 2, 3]
 # k = 1, Output: [1]
 num2 = [1]
 
-# K = 2, Output: [2, 3]
+# K = 2, Output: [5, 3]
 num3 = [1, 2, 2, 3, 3, 3, 4, 5, 5, 5, 5, 6]
 
 test.topKFrequent(num1, 2)
---
+++ Arrays & Hashing/6-Product-of-Array-Except-Self.py
@@ -0,0 +1,73 @@
+# Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+# The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+# You must write an algorithm that runs in O(n) time and without using the division operation. This means naive brute force method won't work.
+
+from typing import List
+
+class Solution(object):
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # Can be split into 2 arrays, one carrying the multiplication before nums[i] and one carrying multiplication after nums[i]
+        #    [ 1 , 2 , 3 , 4]       <- nums
+        # [1 , 1 , 2 , 6 , 24]      <- forward multiplcation -- one for loop
+        #    [ 24, 24, 12, 4 , 1] <- backward multiplcation -- second for loop
+        # answer[i] = forward[i] * backward[i + 1] -- third for loop
+        # O(3N + 2) or O(N) time
+
+        foward = [1] # make an array to hold foward multiplication
+        for i in range(len(nums)):
+            previousForward = foward[i]
+            number = nums[i]
+            multi = previousForward * number
+            foward.append(multi)
+        # print(f"{foward}")
+        backward = [1] # make array to hold backward multiplication
+        for i in range(len(nums) - 1, -1 , -1):
+            previousBackward = backward[0]
+            number = nums[i]
+            multi = previousBackward * number
+            # insert multi as first element and push everything back
+            backward.insert(0, multi) 
+        # print(f"{backward}")
+        output = [] # make array for output as answer[i]
+        for i in range(len(nums)):
+            multi = foward[i] * backward[i + 1]
+            output.append(multi)
+
+        # print(f"{output}")
+        return output
+
+class Solution(object):
+    def neetProductExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        answer = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            answer[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            answer[i] *= postfix
+            postfix *= nums[i]
+        return answer
+
+
+test = Solution()
+
+nums1 = [1,2,3,4]
+# Output: [24,12,8,6]
+
+nums2 = [-1,1,0,-3,3]
+# Output: [0,0,9,0,0]
+
+test.productExceptSelf(nums1)
+test.productExceptSelf(nums2)
