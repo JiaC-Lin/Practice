1292ab2a21169e99ca6e83d28468ea68ff49de67
JiaC-Lin <jiacong.d.lin@gmail.com>
2023-04-26 01:41:32 -0400

    valid palindromes with pointers for memory

--- Two Pointers/1-Valid-Palindrome.py
+++ Two Pointers/1-Valid-Palindrome.py
@@ -3,24 +3,54 @@
 # Given a string s, return true if it is a palindrome, or false otherwise.
 
 class Solution():
+    # SAVE MEMORY, instead of creating a temporary holder for string, using an in-place method with POINTERS on left and right to check if alphanumerical elements are the same 1-by-1
     def isPalindrome(self, s: str) -> bool:
         """
         :type s: str
         :rtype: bool
         """
-        lowercase = s.lower()
+        left = 0
+        right = len(s) - 1
+
+        while left < right:
+            # Check if element is not alphanumerical, while within range, if not then move the pointer to the next element
+            while (left < right) and (self.isAlphanumeric(s[left]) == False):
+                left += 1
+            while (left < right) and (self.isAlphanumeric(s[right]) == False):
+                right -= 1
+            # Stop if found fault with supposed palindrome; make letters to lowercase since we don't care about case
+            if s[left].lower() != s[right].lower():
+                print("False")
+                return False
+            left += 1
+            right -= 1
+        # True if we went through whole string and has no faults at being palindrome
+        print("True")
+        return True
+        
+
+    # implementation of isalnum() function to check if an element is either (A-Z), (a-z) or (0-9)
+    def isAlphanumeric(self, c: chr) -> bool:
+        """
+        :type s: str
+        :rtype: bool
+        """
+        if (ord("A") <= ord(c) <= ord("Z") or 
+            ord("a") <= ord(c) <= ord("z") or 
+            ord("0") <= ord(c) <= ord("9")):
+            return True
+        else:
+            return False
     
 test = Solution()
 
 s1 = "A man, a plan, a canal: Panama"
 # Output: true
 # Explanation: "amanaplanacanalpanama" is a palindrome.
-# Example 2:
 
 s2 = "race a car"
 # Output: false
 # Explanation: "raceacar" is not a palindrome.
-# Example 3:
 
 s3 = " "
 # Output: true
